038a42e93d32af2688a3f00962e247e08a56ed1c
gammaseeker <joeyjiem@gmail.com>
2021-09-22 11:10:54 -0400

    created test sql script

---
+++ __tests__/test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO organization VALUES (DEFAULT, "test1", "2021-01-01", 100, "true");
+INSERT INTO organization VALUES (DEFAULT, "test2", "2021-02-01", 200, "false");
+INSERT INTO organization VALUES (DEFAULT, "test3", "2021-03-01", 300, "true");
+INSERT INTO organization VALUES (DEFAULT, "test4", "2021-04-01", 400, "false");
+INSERT INTO organization VALUES (DEFAULT, "test5", "2021-05-01", 500, "true");
